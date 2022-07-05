21f64cb7c01e2b73868b369b4bc3ffe156334079
jbcsantos028 <jbcsantos028@gmail.com>
2022-07-05 14:48:15 +0800

    Add Main Focus Feature

--- scripts/quote.js
+++ scripts/quote.js
@@ -13,9 +13,9 @@ const setQuote = () => {
         "“Life has got all those twists and turns. You’ve got to hold on tight and off you go.”",
         "“Keep your face always toward the sunshine, and shadows will fall behind you.”\n" +
         "— Walt Whitman",
-        "“Be courageous. Challenge orthodoxy. Stand up for what you believe in. When you are in your rocking chair talking to your grandchildren many years from now, be sure you have a good story to tell.”\n" +
+        "“Be courageous. Challenge orthodoxy. Stand up for what you believe in.”\n" +
         "— Amal Clooney",
-        "“You make a choice: continue living your life feeling muddled in this abyss of self-misunderstanding, or you find your identity independent of it. You draw your own box.”\n" +
+        "“You make a choice: continue living your life feeling muddled in this abyss of self-misunderstanding.”\n" +
         "— Duchess Meghan",
         "“Success is not final, failure is not fatal: it is the courage to continue that counts.”\n" +
         "– Winston Churchill",
